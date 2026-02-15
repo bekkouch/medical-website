f056c60b02685ede00e6034fee7d9a73f802b821
Abderrahmane Bekkouch <abderrahmane.bekkouch@thalesgroup.com>
2026-02-15 13:05:57 +0100

    repo name mistake

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/medical-website', 
+  base: '/medical-website/', 
   server: {
     host: "::",
     port: 8080,
